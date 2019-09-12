0f01999471de37cf9acf188e274d6ea600ed2be9
Shota Iwamoto <amaginqoo@gmail.com>
2019-09-12 09:41:06 -0700

    Add UPDATE and DELETE backend api

--- routes/api/rooms.js
+++ routes/api/rooms.js
@@ -1,18 +1,17 @@
 const experss = require('express');
 const router = experss.Router();
 
-let Room = require('../../model/rooms.model.js')
+let Rooms = require('../../model/rooms.model.js')
 
 router.get('/', (req, res) => {
-  console.log("hello")
-  Room.find()
+  Rooms.find()
     .then(room => res.json(room))
     .catch(err => res.status(400).json(`Error: ${err}`))
 })
 
 router.post('/', (req, res) => {
   const { title, address, roomtype, privateroom, userinfo } = req.body
-  const newRoom = new Room({
+  const newRooms = new Rooms({
     title,
     address,
     roomtype,
@@ -23,12 +22,31 @@ router.post('/', (req, res) => {
       useremail: userinfo.useremail,
     }
   })
-  console.log(newRoom)
-
-  newRoom.save()
+  newRooms.save()
     .then(() => res.json({ "msg": "Room is successfully added"}))
     .catch(err => res.status(400).json(`Error: ${err}`))
-  
+})
+
+router.put('/:id', (req, res) => {
+  const { title, address, roomtype, privateroom, userinfo } = req.body
+  Rooms.findById(req.params.id)
+    .then(room => {
+      room.title = title,
+      room.address = address,
+      room.roomtype = roomtype,
+      room.privateroom = privateroom
+      room.userinfo = userinfo
+
+      room.save()
+        .then(() => res.json('Room updated'))
+        .catch(err => res.status(400).json('Error: ' + err))
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  Rooms.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Room is deleted!'))
+    .catch(err => res.status(400).json({ "error": {err} }))
 })
 
 module.exports = router
