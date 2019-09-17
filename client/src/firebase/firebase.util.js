815d6b90307bb257c3c524c4558158521e253944
yasuhiro <yasucom4646@gmail.com>
2019-09-16 18:06:31 -0700

    Added code for set them on FIrebase

--- client/src/firebase/firebase.util.js
+++ client/src/firebase/firebase.util.js
@@ -3,6 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
+    apiKey: "AIzaSyCCImeJumftjlTu-l8Wpey5kCb3DOy5oVs",
+    authDomain: "find-19b2c.firebaseapp.com",
+    databaseURL: "https://find-19b2c.firebaseio.com",
+    projectId: "find-19b2c",
+    storageBucket: "",
+    messagingSenderId: "23825691849",
+    appId: "1:23825691849:web:16f9bbd636f82916990918"
 }
 
 firebase.initializeApp(config);
@@ -15,8 +22,21 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     const snapShot = await userRef.get();
 
     if(!snapShot.exists) {
-
+        const {displayName, email } = userAuth;
+        const createAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createAt,
+                ...additionalData
+            });
+        } catch(error) {
+            console.log(error.message);
+        }
     }
+
+    return userRef;
 }
 
 export const auth = firebase.auth();
